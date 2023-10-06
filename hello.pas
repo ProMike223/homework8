c41dba7c5448e67fd659008006ae8ebb74e069e1
Mikhail <hudih@mail.ru>
2023-10-06 16:26:00 +0300

    hello.pas changed to support 3 languages

---
+++ hello.pas
@@ -0,0 +1,16 @@
+﻿begin
+  var lang_num:=ReadInteger('Enter your language(1 - ru, 2 - en, 3 - esp): ');
+  while((lang_num > 3) or (lang_num < 1)) do
+    lang_num := ReadInteger('Invalid input. Please, try again: ');
+  
+  var greeting:= '';
+  
+  if lang_num = 1 then
+    greeting := 'Здравствуйте'
+  else if lang_num = 2 then
+    greeting := 'Hello'
+  else
+    greeting := 'Hola';
+   
+   Println(greeting);
+end.
